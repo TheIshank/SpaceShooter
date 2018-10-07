c44579dc2369fb905b52d92effb46ea8684ade86
Ishank Saxena <ish.sxn@gmail.com>
2018-10-08 01:08:58 +0530

    Boundary

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
 public class MovementScript : MonoBehaviour {
 	public float moveSpeed;
+	public Boundary boundary;
 	private Rigidbody rb;
-
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
@@ -15,5 +19,10 @@ public class MovementScript : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.velocity = movement*moveSpeed;
+		rb.position = new Vector3(
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
 	}
 }
